3a6f553c3735141f73f9eb1fc03bdf97cf4949b3
Marc Zdon <marczdon@gmail.com>
2023-03-24 12:48:48 -0400

    Downgrade react-native-reanimated v3.0.2 -> v2.14.4 to fix Android compilation

--- test/setup.ts
+++ test/setup.ts
@@ -1,9 +1,7 @@
 const mockSafeAreaContext =
   require('react-native-safe-area-context/jest/mock').default;
 
-jest.mock('react-native-reanimated', () =>
-  require('react-native-reanimated/mock'),
-);
+require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 jest.mock('react-native-paper', () => {
   const actual = jest.requireActual('react-native-paper');
