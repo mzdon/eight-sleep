eb0cf00766c619c455b9355135e7c50e1bb99475
Marc Zdon <marczdon@gmail.com>
2023-03-24 10:24:36 -0400

    Fix unit tests

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,9 @@
-import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+const mockSafeAreaContext =
+  require('react-native-safe-area-context/jest/mock').default;
 
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 jest.mock('react-native-paper', () => {
   const actual = jest.requireActual('react-native-paper');
